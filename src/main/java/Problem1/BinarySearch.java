d27bd6bf3203aeb8ba5739954bdeb86ba7df3e14
Dani <shaykhodani@gmail.com>
2020-10-31 16:50:29 -0700

    Finished Binary Search

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,22 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        return binaryHelper(data, target, 0, data.length - 1);
+    }
+
+    public static int binaryHelper(int[] array, int search, int start, int end) {
+        int middle = (end + start) / 2;
+
+        if(end < start) {
+            return -1;
+        }
+
+        if (search == array[middle]) {
+            return middle;
+        } else if(search < array[middle]) {
+            return binaryHelper(array, search, start, middle - 1);
+        } else {
+            return binaryHelper(array, search, middle + 1, end);
+        }
     }
 }
